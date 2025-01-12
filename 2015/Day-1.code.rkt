ee0c8dde4c6e8f03bbef9c078b8696099fcc28ed
Jonas Brüll <jonas.bruell@gmail.com>
2025-01-12 16:55:10 +0100

    2015, day 1, challenge 2

--- 2015/Day-1.code.rkt
+++ 2015/Day-1.code.rkt
@@ -11,3 +11,13 @@
           ((eq? #\) (car directions)) (loop (cdr directions) (- floor 1))))))
 
 (santa-floor-calculator confusing-directions) ; 138
+
+; https://adventofcode.com/2015/day/1#part2
+(define (santa-first-time-in-basement-calculator confusing-directions)
+  (let loop ((directions confusing-directions) (floor 0) (position 0))
+    (cond ((null? directions) position)
+          ((< floor 0) position)
+          ((eq? #\( (car directions)) (loop (cdr directions) (+ floor 1) (+ position 1)))
+          ((eq? #\) (car directions)) (loop (cdr directions) (- floor 1) (+ position 1))))))
+
+(santa-first-time-in-basement-calculator confusing-directions) ; 1771
