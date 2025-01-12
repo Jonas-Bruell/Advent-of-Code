b2ff27833f04251a58679d54f0ec11ed74502cca
Jonas Brüll <jonas.bruell@gmail.com>
2025-01-12 17:17:28 +0100

    2015, day 2, challenge 2

--- 2015/Day-2.code.rkt
+++ 2015/Day-2.code.rkt
@@ -3,8 +3,7 @@
 (require "../parser.rkt" "../config.rkt")
 (define list-of-packages (parse "./Day-2.data.txt" to-strings newline))
 
-; https://adventofcode.com/2015/day/2
-(define (calculate-square-feet-of-wrapping-paper list-of-packages)
+(define (higher-order-loop list-of-packages function)
   (let loop ((packages list-of-packages) (sum 0))
     (if (null? packages)
         sum
@@ -12,8 +11,24 @@
                              (string-split (car packages) "x")))
                (l (car package)) (w (cadr package)) (h (caddr package)))
           (loop (cdr packages)
-                (+ sum (+ (* 2 l w) (* 2 w h) (* 2 h l)
-                          ((λ (x) (* (car x) (cadr x)))
-                           (remove (max l w h) package)))))))))
+                (function package sum l w h))))))
+
+; https://adventofcode.com/2015/day/2
+(define (calculate-square-feet-of-wrapping-paper list-of-packages)
+  (higher-order-loop
+   list-of-packages
+   (λ (package sum l w h)
+     (+ sum (+ (* 2 l w) (* 2 w h) (* 2 h l)
+               ((λ (x) (* (car x) (cadr x))) (remove (max l w h) package)))))))
 
 (calculate-square-feet-of-wrapping-paper list-of-packages)
+
+; https://adventofcode.com/2015/day/2#part2
+(define (calculate-feet-of-ribbon list-of-packages)
+  (higher-order-loop
+   list-of-packages
+   (λ (package sum l w h)
+     (+ sum (* l w h)
+        ((λ (x) (* 2 (+ (car x) (cadr x)))) (remove (max l w h) package))))))
+
+(calculate-feet-of-ribbon list-of-packages)
