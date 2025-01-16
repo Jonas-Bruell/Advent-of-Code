c692d007c55731ba8dffa2b113e30f76e47cd583
Jonas Brüll <jonas.bruell@gmail.com>
2025-01-16 01:53:15 +0100

    2015, day 4, challenge 2

--- 2015/Day-4.code.rkt
+++ 2015/Day-4.code.rkt
@@ -4,14 +4,17 @@
 (define secret-key-file (parse "./Day-4.data.txt" to-strings newline))
 
 ; https://adventofcode.com/2015/day/4
-(define (adventcoins-miner secret-key)
+(define (adventcoins-miner secret-key compare)
   (let loop ((index 0))
     (let* ((hash (md5 (open-input-string
                        (string-append secret-key (number->string index)))))
-           (first-five-numbers-hash (substring hash 0 5)))
-      (if (equal? first-five-numbers-hash "00000")
+           (first-five-numbers-hash (substring hash 0 (string-length compare))))
+      (if (equal? first-five-numbers-hash compare)
           index
           (loop (+ index 1))))))
 
 ; secret key is first element of list
-(adventcoins-miner (car secret-key-file)) ; 346386
+(adventcoins-miner (car secret-key-file) "00000") ; 346386
+
+; https://adventofcode.com/2015/day/4#part2
+(adventcoins-miner (car secret-key-file) "000000") ; 9958218
